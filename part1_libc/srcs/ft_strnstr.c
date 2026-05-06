a1d38a609a9b87b98343f60d7bcded3eaf203a8e
Julia Alkhimova <devalchem@gmail.com>
2026-05-06 20:07:01 +0200

    feat: implement ft_strtrim function to part_2

--- part1_libc/srcs/ft_strnstr.c
+++ part1_libc/srcs/ft_strnstr.c
@@ -1,18 +1,9 @@
-// NAME
-
-
-// DESCRIPTION
-
-
-// RETURN VALUES
-
 #include "libft.h"
 
 char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
     size_t i;
     size_t needle_len;
-    
     
     needle_len = ft_strlen(needle);
     if (needle_len == 0)
