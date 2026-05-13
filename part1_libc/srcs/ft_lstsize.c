1909938b44d6d8c68ea0a15c7a1011a28d5a3dca
Julia Alkhimova <devalchem@gmail.com>
2026-05-13 16:13:54 +0200

    feat: finish ft_split, start lst functions

---
+++ part1_libc/srcs/ft_lstsize.c
@@ -0,0 +1,13 @@
+int ft_lstsize(t_list *lst)
+{
+    int count;
+    t_list *current;
+    count = 0;
+    current = lst;
+    while(current)
+    {
+        current = current->next;
+        count++;
+    }
+    return (count);
+}
--- part1_libc/srcs/ft_split.c
+++ part1_libc/srcs/ft_split.c
@@ -1,34 +1,90 @@
-
-
-
-char **ft_split(char const *s, char c);
-
-int	ft_count_words(const char *s, char c)
+#include <stdio.h>
+#include <stdlib.h>
+size_t count_words(const char *str, char limit)
 {
-	int	i;
-	int	words;
-
-	i = 0;
-	words = 0;
-	while (s[i]) // recorremos el el stiring hasta el final 
-	{
-		if (s[i] != c) // si el valor de s[i] es difrente al c, contamos 1 palabra 
-		{
-			words++;
-	        while (s[i] && s[i] != c) // entiendo que aqui sigue recorriendo dentro del substring hasta que no se de con el c y entoces sale del bucle interno y vuelve a entrar en el bucle extreno porque aaun no he recorrido tyodo el string
-	            i++;
-		} 	
-		else // si el valor de s[i] es igual al c, contamos saltamos al siguente indice (como que haciendo skip)
-			i++;
-	}
-	return (words);
+    size_t i;
+    size_t count;
+    i = 0;
+    count = 0;
+    while(str[i])
+    {
+        if(str[i] != limit)
+        {
+            count++;
+            while(str[i] && str[i] != limit)
+                i++;
+        } 
+        else
+            i++;
+    }
+    return count;
+}  
+char *extract_word(const char *s, char limit)
+{
+    size_t i;
+    char *word;
+    i = 0;
+    while(s[i] && s[i] != limit)
+        i++;
+    word = malloc(i+1);
+    if(!word)
+        return NULL;
+    i = 0;
+    while(s[i] && s[i] != limit)
+       {
+        word[i] = s[i];
+        i++;
+       }   
+    word[i] = '\0'; 
+    return (word);
 }
-
+char **ft_split(char const *s, char c)
+{
+    int i;
+    int j;
+    char **words;
+    
+    i = 0;
+    j = 0;
+    if (!s)
+        return NULL;
+    words = malloc(sizeof(char*) * (count_words(s, c)+1)); 
+    if(!words)
+        return NULL;
+    while(s[i])
+    {
+        if(s[i] != c)
+        {
+            words[j] = extract_word(&s[i], c);
+            if (!words[j]) // ✅ si falla, liberar todo
+            {
+                while (j > 0)
+                    free(words[--j]);
+                free(words);
+                return NULL;
+            }
+            while(s[i] && s[i] != c)
+                i++;
+            j++;
+        }
+        else
+            i++;
+    }
+    words[j] = NULL; // ✅ mejor NULL explícito que 0
+    return (words);
+}
 int main() {
-    char *str = "Hello  Wolrd !";
-    char separator = ' ';
-    ft_count_words(str, separator);
+    char *str = "Hello World";
+    printf("%s", str);
+    
+    char **arr = ft_split(str, ' ');
+    int i = 0;
+    while (arr[i])
+    {
+        printf("\n|%s|", arr[i]);
+        i++;
+    }
+     printf("\nChar: %d", sizeof(char));  
+     printf("\nPointer: %d", sizeof(char*));
     return 0;
 }
-
-// https://github.com/waltergcc/42-libft/blob/main/libft/ft_split.c
